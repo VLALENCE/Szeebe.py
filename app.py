8f2f587b5019fd747edfe5ab6d2952817d14e03a
Scrippy <50034413+Scrippy@users.noreply.github.com>
2021-07-22 15:49:29 -0500

    app.py Created

---
+++ app.py
@@ -0,0 +1,13 @@
+print(('-' * 23) + '\nTest build of Szeebe.py\nCreated by: Scrippy\nStarted Jul. 22, 2021\n' + ('-' * 23))
+
+is_Active = True
+
+if is_Active:
+    print('Szeebe is able to run.')
+    print('-' * 23)
+else:
+    print('Szeebe is unable to run.')
+    print('-' * 23)
+    quit()
+
+
