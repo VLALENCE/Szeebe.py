25265415658a1e6b3ecf4470b25e26983cc419c2
Scrippy <50034413+Scrippy@users.noreply.github.com>
2021-07-22 20:40:21 -0500

    app.py works with discord integration now.

--- app.py
+++ app.py
@@ -19,4 +19,29 @@ load_dotenv()
 TOKEN = os.getenv('TOKEN')
 PREFIX = os.getenv('PREFIX')
 
-print(PREFIX)
+import discord
+
+class MyClient(discord.Client):
+        async def on_ready(self):
+            print('Logged in as...')
+            print(self.user.name)
+            print(self.user.id)
+            print('-' * 23)
+
+        async def on_message(self, message):
+            if message.author.id == self.user.id:
+                return
+
+            if message.content.lower() == 'hi':
+                await message.channel.send('Hello there')
+                return
+            if message.content.lower() == 'hello there':
+                await message.channel.send('General Kenobi')
+                return
+
+            if message.content.lower() == 'general kenobi':
+                await message.channel.send('You are a bold one')
+
+
+client = MyClient()
+client.run(TOKEN)
