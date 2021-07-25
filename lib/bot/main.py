a696c3bd48105a802bff6ae88fc68b2e81608832
Scrippy <50034413+Scrippy@users.noreply.github.com>
2021-07-25 14:06:57 -0500

    First working module added.

---
+++ lib/bot/main.py
@@ -0,0 +1,15 @@
+# By ScriptIntelligence
+# Started Jul. 25, 2021
+
+# // Libaries
+
+# // Modules
+
+import env_variables
+
+# // Variables
+
+TOKEN = env_variables.token()
+PREFIX = env_variables.prefix()
+
+print(PREFIX)
