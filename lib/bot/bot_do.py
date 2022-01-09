a2cf2daa189553da3d6cac2af2a1df2d6c4e54e1
Scrippy <50034413+Scrippy@users.noreply.github.com>
2022-01-09 10:31:39 -0600

    Commented / Archived old createTriggerWordResponse, renamed new createTriggerWordResponseNew to createTriggerWordResponse

--- lib/bot/bot_do.py
+++ lib/bot/bot_do.py
@@ -37,25 +37,24 @@ async def status_task(self):
         await self.change_presence(status=discord.Status.online, activity=activity)
         await asyncio.sleep(10)
 
+#def createTriggerWordResponse(message):
+#    if message.content.lower() == 'hi': return message.channel.send('Hello there')
+#    if message.content.lower() == 'hello there': return message.channel.send('General Kenobi')
+#    if message.content.lower() == 'general kenobi': return message.channel.send('You are a bold one')
+#    if message.content.lower().find('birthday') >= 0: return message.channel.send('Happy Birthday!!!')
+#    if message.content.lower().find('scrizeebe') >= 0: return message.channel.send('Script*')
+#    if message.content.lower().find('boing') >= 0: return message.channel.send('boing')
+#    if message.content.lower().find('frog') >= 0: return message.channel.send('boing ribbit boing')
+#    if message.content.lower() == 'no.': return message.channel.send(files=discord.File('https://github.com/Scrippy/conch.rbx/raw/main/Audio/no.mp3'))
+#    if message.content.lower() == 'nothing.': return
+#    if message.content.lower() == 'try asking again.': return
+#    if message.content.lower() == 'i dont think so': return
+#    if message.content.lower() == 'i don\' think so': return
+#    if message.content.lower() == 'maybe some day.': return
+#    if message.content.lower() == 'yes.': return
+
+
 def createTriggerWordResponse(message):
-    if message.content.lower() == 'hi': return message.channel.send('Hello there')
-    if message.content.lower() == 'hello there': return message.channel.send('General Kenobi')
-    if message.content.lower() == 'general kenobi': return message.channel.send('You are a bold one')
-    if message.content.lower().find('birthday') >= 0: return message.channel.send('Happy Birthday!!!')
-    if message.content.lower().find('scrizeebe') >= 0: return message.channel.send('Script*')
-    if message.content.lower().find('boing') >= 0: return message.channel.send('boing')
-    if message.content.lower().find('frog') >= 0: return message.channel.send('boing ribbit boing')
-
-    #if message.content.lower() == 'no.': return message.channel.send(files=discord.File('https://github.com/Scrippy/conch.rbx/raw/main/Audio/no.mp3'))
-    #if message.content.lower() == 'nothing.': return
-    #if message.content.lower() == 'try asking again.': return
-    #if message.content.lower() == 'i dont think so': return
-    #if message.content.lower() == 'i don\' think so': return
-    #if message.content.lower() == 'maybe some day.': return
-    #if message.content.lower() == 'yes.': return
-
-
-def createTriggerWordResponseNew(message):
     normalResponseFiles = os.listdir(r"lib\bot\responses\normal")
     for file in normalResponseFiles:
         if file.endswith('.py'):
